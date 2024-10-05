8eb3b76b965efb50c021c38a126e62864f106fce
Carol <carolduarte2000@gmail.com>
2024-10-04 22:13:26 -0300

    Implementa opção de depósito

--- CaixaEletronico/Program.cs
+++ CaixaEletronico/Program.cs
@@ -1,21 +1,22 @@
-﻿decimal saldo = 1000;
+﻿int saldo = 1000;
 
 inicio:
-Console.WriteLine("- Caixa Eletrônico -");
+Console.WriteLine("CAIXA ELETRÔNICO");
 Console.WriteLine("[1] Saque");
 Console.WriteLine("[2] Depósito");
 Console.WriteLine("[3] Transferência");
 Console.WriteLine("[4] Saldo e Extrato");
 Console.WriteLine("[0] Sair");
+escolha:
 Console.Write("Por favor, insira o número correspondente à opção desejada: ");
 string escolhaUsuario = Console.ReadLine();
 
 switch (escolhaUsuario)
 {
     case "1":
-        Console.WriteLine("- Saque -");
+        Console.WriteLine("SAQUE");
         Console.Write("Digite o valor: ");
-        decimal valorSaque = decimal.Parse(Console.ReadLine());
+        int valorSaque = int.Parse(Console.ReadLine());
         if (saldo >= valorSaque)
         {
             saldo -= valorSaque;
@@ -24,13 +25,24 @@ switch (escolhaUsuario)
         }
         else
         {
-            Console.WriteLine("Saldo insuficiente!");            
+            Console.WriteLine("Saldo insuficiente!");
         }
         break;
+    case "2":
+        Console.WriteLine("DEPÓSITO");
+        Console.Write("Digite o valor: ");
+        int valorDeposito = int.Parse(Console.ReadLine());
+        saldo += valorDeposito;
+        Console.WriteLine($"Você depositou {valorDeposito:C}.");
+        Console.WriteLine($"Saldo atual: {saldo:C}.");
+        break;
+    default:
+        Console.WriteLine("Opção inválida. Tente novamente.");
+        goto escolha;
 }
+
 Console.Write("Deseja fazer outra operação? (S/N): ");
 escolhaUsuario = Console.ReadLine().ToUpper();
-
 if (escolhaUsuario == "S")
 {
     goto inicio;
